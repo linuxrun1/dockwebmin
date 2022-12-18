6a4e31a7cab9072a3e29ee7733cf77473ea16629
I'm Unix <ferdi@ferdiwardiyan.eu.org>
2022-12-18 08:13:01 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const app = express();
-
-function startWithRetry() {
-    app.listen(8080, () => {
-      app.get("/", (req, res, next) => {
-        res.sendStatus(200)
-        res.end('OK')
-        return;
-      });
+app.listen(8080, () => {
+  app.get("/", (req, res) => {
+    res.sendStatus(200)
+      res.end('OK')
+     });
       console.log("Server running on port 8080.");
-    });
-}
-
-startWithRetry();
+});
